ace52460b4bce3e83fceda3885045cb2c0831ee1
Vladimir Modenov <vmodenov@gmail.com>
2020-06-28 17:54:33 +0300

    Add "Credit.py" file!

---
+++ 5-Credit.py
@@ -0,0 +1,9 @@
+# Calculating of credit final sum
+
+print('Сейчас расчитаем ваш кредит')
+need_sum = int(input('Введите желаемую сумму: '))
+numbers_of_years = int(input('На сколько лет хотите взять кредит? '))
+credit_rate = int(input('Введите процентную ставку банка: '))
+final_sum = need_sum + need_sum / 100 * credit_rate * numbers_of_years
+
+print(f'Платить придётся {final_sum} рублей')
