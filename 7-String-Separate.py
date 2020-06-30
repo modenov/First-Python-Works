e58ef642fef9524fc2cf1206a1daf04c36ed8e18
Vladimir Modenov <vmodenov@gmail.com>
2020-06-30 21:29:24 +0300

    Add some new files.

---
+++ 7-String-Separate.py
@@ -0,0 +1,11 @@
+# Разделяем строку по определённому символу
+
+data = "12;10;8;10"
+separated_data = data.split(';')
+print(separated_data)
+print('И класс у нас ' + str(type(separated_data)) + ' получается!')
+
+# Также можно ФИО разделять по пробелу!
+my_name = "Владимир Романович Моденов"
+separated_my_name = my_name.split(' ')
+print(separated_my_name)
